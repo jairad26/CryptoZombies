24bc46e6fda4f304200a0ab3a274577e7136107e
jairad26 <jairad26@gmail.com>
2021-12-29 09:31:54 -0800

    finished cryptozombies course

--- zombieownership.sol
+++ zombieownership.sol
@@ -12,11 +12,11 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 
   mapping (uint => address) zombieApprovals;
 
-  function balanceOf(address _owner) override external view returns (uint256) {
+  function balanceOf(address _owner)  external view override returns (uint256) {
     return ownerZombieCount[_owner];
   }
 
-  function ownerOf(uint256 _tokenId) override external view returns (address) {
+  function ownerOf(uint256 _tokenId) external view override returns (address) {
     return zombieToOwner[_tokenId];
   }
 
@@ -27,12 +27,12 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     emit Transfer(_from, _to, _tokenId);
   }
 
-  function transferFrom(address _from, address _to, uint256 _tokenId) override external payable {
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable override {
     require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
     _transfer(_from, _to, _tokenId);
   }
 
-  function approve(address _approved, uint256 _tokenId) override external payable onlyOwnerOf(_tokenId) {
+  function approve(address _approved, uint256 _tokenId) external payable override onlyOwnerOf(_tokenId) {
     zombieApprovals[_tokenId] = _approved;
     emit Approval(msg.sender, _approved, _tokenId);
   }
